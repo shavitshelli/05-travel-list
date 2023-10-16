9db3c93122865a4213984c756ff3a7ef1451a74d
Shavit Shelli <shavit.shelli57@gmail.com>
2023-10-16 12:23:15 +0300

    added a dynamic items arr instead of initialItems

--- src/App.js
+++ src/App.js
@@ -9,11 +9,20 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    //In react were not allowed to mutate state thus we cant push item to items array
+    // The problem derives from the fact that useState need to get a new object to determine whether
+    // it needs to re-render the component or not and the push method does not return a new object
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -22,37 +31,39 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   //The reason for us to want to use controlled elements is to be able to control all
   //the elements from inside react amd not be controled from within the DOM therfore we need state
   // to maintain the application in sync with the changes occuring
   //1)first we use useState to get the curr state of the description
   //2)second we give the input tag the value of desc
   //3)third onChange we set the text to be the value entered - e.target.value
-  const [desc, setDesc] = useState("");
-  const [numItems, setNumItems] = useState(1);
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     //prevents from the page being reloaded on submit
     e.preventDefault();
 
     //if no description exist we dont need to submit
-    if (!desc) return;
+    if (!description) return;
 
-    const newItem = { desc, numItems, packed: false, id: Date.now() };
-    console.log(newItem);
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    // console.log(newItem);
+
+    onAddItems(newItem);
 
     //afet submitting we want to go back to initial state
-    setDesc("");
-    setNumItems(1);
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip 😍</h3>
       <select
-        value={numItems}
-        onChange={(e) => setNumItems(Number(e.target.value))}
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
       >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((opt) => (
           <option value={opt} key={opt}>
@@ -63,18 +74,18 @@ function Form() {
       <input
         type="text"
         placeholder="Item..."
-        value={desc}
-        onChange={(e) => setDesc(e.target.value)}
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       />
       <button>Add</button>
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
